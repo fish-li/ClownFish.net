68b8063c8927f56af9aad5af38162f69bec68cdd
fish-li <liqifeng0503@163.com>
2016-03-12 22:48:06 +0800

    Add del_bin_dirs.cmd

---
+++ del_bin_dirs.cmd
@@ -0,0 +1,16 @@
+rd "demo\web\CorsClientWebSite1\bin" /q /s
+rd "demo\web\DEMO.BLL\bin" /q /s
+rd "demo\web\DEMO.Common\bin" /q /s
+rd "demo\web\DEMO.Controllers\bin" /q /s
+rd "demo\web\DEMO.Model\bin" /q /s
+rd "demo\web\MvcDemoWebSite1\bin" /q /s
+rd "demo\web\ReverseProxyWebSite\bin" /q /s
+rd "src\ClownFish.Base\bin" /q /s
+rd "src\ClownFish.Log\bin" /q /s
+rd "src\ClownFish.MockAspnetRuntime\bin" /q /s
+rd "src\ClownFish.Web\bin" /q /s
+rd "test\ClownFish.Log.UnitTest\bin" /q /s
+rd "test\ClownFish.Web.UnitTest\bin" /q /s
+rd "test\TestApplication1\bin" /q /s
+
+pause
