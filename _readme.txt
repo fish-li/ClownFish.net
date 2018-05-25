9dc2d4ba411a92aae5c6ff22658dc78e461ddea7
Fish Li（李奇峰） <liqifeng0503@163.com>
2018-05-25 11:06:34 +0800

    增加性能测试项目

---
+++ _readme.txt
@@ -0,0 +1,7 @@
+
+ClownFish.Data.PerformanceTest，是针对ClownFish.Data的性能测试项目
+
+
+PerformanceTest.Handlers，是采用ClownFish.Web开发的HTTP处理器项目，
+测试时，将 \ptest\PerformanceTest.Website 部署成站点即可。
+
---
+++ ptest/PerformanceTest.Handlers/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClownFish.Web;
+
+namespace PerformanceTest.Handlers
+{
+    public sealed class Class1Controller : BaseController
+    {
+        [PageUrl(Url = "/hello/add.aspx")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
