f3fb4dbe2536949182def92f134edc1f2aadc33c
Fish Li（李奇峰） <liqifeng0503@163.com>
2018-06-22 18:29:07 +0800

    优化异常日志中SQL命令参数的序列化

--- src/ClownFish.Log/Model/SqlInfo.cs
+++ src/ClownFish.Log/Model/SqlInfo.cs
@@ -4,6 +4,7 @@ using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using ClownFish.Base;
 using ClownFish.Base.Xml;
 using ClownFish.Log;
@@ -11,10 +12,45 @@ using ClownFish.Log;
 
 namespace ClownFish.Log.Model
 {
-	/// <summary>
-	/// 表示SQL执行相关的消息数据结构
-	/// </summary>
-	public class SqlInfo
+    /// <summary>
+    /// 表示一组用于HTTP传输的 【名称/值】 对。
+    /// </summary>
+    [Serializable]
+    public sealed class DbParam
+    {
+        /// <summary>
+        /// 参数名称
+        /// </summary>
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 参数类型
+        /// </summary>
+        [XmlAttribute]
+        public string DbType { get; set; }
+
+        /// <summary>
+        /// 参数值
+        /// </summary>
+        public string Value { get; set; }
+
+        
+        /// <summary>
+        /// ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("{0}={1}", this.Name, this.Value);
+        }
+    }
+
+
+    /// <summary>
+    /// 表示SQL执行相关的消息数据结构
+    /// </summary>
+    public class SqlInfo
 	{
 		private static int MaxSqlTextLen = 1024 * 1024;
 
@@ -37,7 +73,7 @@ namespace ClownFish.Log.Model
 		/// <summary>
 		/// 命令参数列表（可能为 NULL）
 		/// </summary>
-		public List<NameValue> Parameters { get; set; }
+		public List<DbParam> Parameters { get; set; }
 
 
 		/// <summary>
@@ -56,27 +92,29 @@ namespace ClownFish.Log.Model
 			if( command.Parameters == null || command.Parameters.Count == 0 )
 				return info;
 
-			info.Parameters = new List<NameValue>(command.Parameters.Count);
+			info.Parameters = new List<DbParam>(command.Parameters.Count);
 
 			for( int i = 0; i < command.Parameters.Count; i++ ) {
 				if( i < MaxParameterCount ) {
 					DbParameter parameter = command.Parameters[i];
 
-					NameValue nv = new NameValue();
-					nv.Name = parameter.ParameterName;
-					if( parameter.Value != null )
-						nv.Value = parameter.Value.ToString().SubstringN(MaxParaValueLen);
-					else
-						nv.Value = "NULL";
+                    DbParam pv = new DbParam();
+					pv.Name = parameter.ParameterName;
+                    pv.DbType = parameter.DbType.ToString();
 
-	
-					info.Parameters.Add(nv);
+					if( parameter.Value == null || parameter.Value == DBNull.Value )
+                        pv.Value = "NULL";
+					else
+                        pv.Value = parameter.Value.ToString().SubstringN(MaxParaValueLen);
+                    
+                    info.Parameters.Add(pv);
 				}
 				else {
-					NameValue nv = new NameValue();
-					nv.Name = "#####";
-					nv.Value = "参数太多，已被截断...，参数数量：" + command.Parameters.Count.ToString();
-					info.Parameters.Add(nv);
+                    DbParam pv = new DbParam();
+					pv.Name = "#####";
+                    pv.DbType = System.Data.DbType.String.ToString();
+                    pv.Value = "参数太多，已被截断...，参数数量：" + command.Parameters.Count.ToString();
+					info.Parameters.Add(pv);
 					break;
 				}
 			}
