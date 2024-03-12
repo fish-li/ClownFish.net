b8b3a8a4917003338b557f18256fac3eec7f72c6
Fish Li <liqifeng0503@163.com>
2024-03-12 15:02:47 +0800

    Http403Handler优化，登录后继续显示原地址

--- src/ClownFish.Web/Handlers/Http403Handler.cs
+++ src/ClownFish.Web/Handlers/Http403Handler.cs
@@ -4,8 +4,11 @@ public sealed class Http403Handler : IAsyncNHttpHandler
 {
     public static readonly Http403Handler Instance = new Http403Handler();
 
-    internal static readonly string LoginUrl = LocalSettings.GetSetting("UserLoginUrl", "/").UrlEncode();
-    internal static readonly string HtmlContent = typeof(Http403Handler).Assembly.ReadResAsText("ClownFish.Web.files.http403-not-login.html");
+    internal static readonly byte[] HtmlContentBytes = typeof(Http403Handler).Assembly.ReadResAsText("ClownFish.Web.files.http403-not-login.html").GetBytes();
+
+    // 像 Kibana 这类程序喜欢用前端路由，产生的URL例如：http://linuxtest:8208/app/discover#/?_g=(time:(from:now-30m,to:now))&xxxxxxx
+    // 此时，在服务端只能取到：http://linuxtest:8208/app/discover
+    // 所以没法在服务端生成回跳链接，所以最终采用在JS在页面中获取当前URL并修改链接的方法来解决
 
     public async Task ProcessRequestAsync(NHttpContext httpContext)
     {
@@ -13,8 +16,8 @@ public sealed class Http403Handler : IAsyncNHttpHandler
         response.StatusCode = 403;
         response.ContentType = ResponseContentType.HtmlUtf8;
 
-        string html = HtmlContent.Replace("##returnUrl##", LoginUrl);
+        //string html = HtmlContent.Replace("##returnUrl##", _url.UrlEncode());
         
-        await response.WriteAllAsync(html.GetBytes());
+        await response.WriteAllAsync(HtmlContentBytes);
     }
 }
