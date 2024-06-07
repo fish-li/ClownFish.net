3d4c0eadcc009015d706f950759c9a8bb9a4cfb7
Fish Li <liqifeng0503@163.com>
2024-06-07 11:11:33 +0800

    update version

--- src/ClownFish.net/ConstValues.cs
+++ src/ClownFish.net/ConstValues.cs
@@ -17,7 +17,7 @@ public static class ConstValues
 
     static ConstValues()
     {
-        CurrentVersion = AsmHelper.GetFileVersion(typeof(ConstValues)).IfEmpty("8.24.605.1");
+        CurrentVersion = AsmHelper.GetFileVersion(typeof(ConstValues)).IfEmpty("8.24.607.1");
 
         ReLoad();
     }
