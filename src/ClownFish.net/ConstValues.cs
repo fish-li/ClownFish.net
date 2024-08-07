0917f90a9a331c63eee1b26443f1786e4888cfae
Fish Li <liqifeng0503@163.com>
2024-08-07 18:12:41 +0800

    update version

--- src/ClownFish.net/ConstValues.cs
+++ src/ClownFish.net/ConstValues.cs
@@ -17,14 +17,14 @@ public static class ConstValues
 
     static ConstValues()
     {
-        CurrentVersion = AsmHelper.GetFileVersion(typeof(ConstValues)).IfEmpty("8.24.807.2");
+        CurrentVersion = AsmHelper.GetFileVersion(typeof(ConstValues)).IfEmpty("8.24.807.3");
 
         ReLoad();
     }
 
     internal static void ReLoad()
     {
-        // 示例：ClownFish.HttpClient/8.24.807.2/TxClientX/cluster1/dev/fish-debian12/Debian GNU-Linux 12 (bookworm)
+        // 示例：ClownFish.HttpClient/8.24.807.3/TxClientX/cluster1/dev/fish-debian12/Debian GNU-Linux 12 (bookworm)
         HttpClientUserAgent = $"ClownFish.HttpClient/{CurrentVersion}/{EnvUtils.ApplicationName}/{EnvUtils.ClusterName}/{EnvUtils.RunEnv}/{EnvUtils.GetHostName()}/{OsUtils.GetOsName().Replace('/', '-')}";
     }
 }
