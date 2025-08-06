e1e12703f91d5d37d93fb9522363b3bc5e219204
Fish Li（李奇峰） <liqifeng0503@163.com>
2025-08-06 14:10:16 +0800

    update version

--- src/ClownFish.net/ConstValues.cs
+++ src/ClownFish.net/ConstValues.cs
@@ -17,9 +17,9 @@ public static class ConstValues
 
     static ConstValues()
     {
-        CurrentVersion = AsmHelper.GetFileVersion(typeof(ConstValues)).IfEmpty("9.25.730.1");
+        CurrentVersion = AsmHelper.GetFileVersion(typeof(ConstValues)).IfEmpty("9.25.806.1");
 
-        // 示例：ClownFish.HttpClient/9.25.730.1
+        // 示例：ClownFish.HttpClient/9.25.806.1
         HttpClientUserAgent = $"ClownFish.HttpClient/{CurrentVersion}";
     }
 
